92ed6f5db34be06ba58e7f7358676000b52a7594
barwidman <bar.widman@post.runi.ac.il>
2025-11-04 12:29:17 +0200

    feat: Added solution to Cheers exercise

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,31 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static final int NUM_OF_ARGS = 2;
+    public static void main(String[]  args) {
+        if (args.length != NUM_OF_ARGS) {
+            System.err.println("Usage: java Cheers <CHEER> <NUM_OF_CHEERS>");
+            return;
         }
+        String cheer = args[0];
+        int numOfCheers = Integer.parseInt(args[1]);
+
+        char[] useAnCharacters = {'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R','S', 'X'};
+
+        for (int i = 0; i < cheer.length(); i++) {
+            String letterPrefix = "a";
+            char currentCharacterUpper = Character.toUpperCase(cheer.charAt(i));
+            for (int j = 0; j < useAnCharacters.length; j++) {
+                if (currentCharacterUpper == useAnCharacters[j]) {
+                    letterPrefix = "an";
+                    break;
+                }
+            }
+            System.out.println("Give me " + letterPrefix + " " +  currentCharacterUpper + ": " + currentCharacterUpper + "!");
+        }
+        System.out.println("What does that spell?");
+
+        for (int i = 0; i < numOfCheers; i++) {
+            System.out.println(cheer.toUpperCase() + "!!!");
+        }
+    }
 }
